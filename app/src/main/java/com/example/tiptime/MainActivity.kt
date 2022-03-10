72b1e249f3d56f65e6ea1abad697e9d6cb180afb
Hussain Aqeel <hussaingoodboi@gmail.com>
2022-03-10 23:58:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.example.tiptime
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
+import com.example.tiptime.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import java.text.DecimalFormat
+import kotlin.math.round
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var costInput: EditText
+    private lateinit var roundupToggle: SwitchCompat
+    private lateinit var tipAmountText: TextView
+
+    private var tipAmount = 0.0
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+
+            costInput = costOfServiceInput
+            roundupToggle = roundupSwitch
+            tipAmountText = tipAmount
+
+            calcButton.setOnClickListener {
+                onCalcButtonClicked(it)
+            }
+        }
+
+
+        roundupToggle.setOnCheckedChangeListener { _, isChecked: Boolean ->
+            onRoundSwitchChanged(
+                isChecked
+            )
+        }
+    }
+
+    private fun onCalcButtonClicked(v: View) {
+        if (costInput.text.toString().trim().isEmpty()) {
+            showSnackbar(v, "Please enter the cost of Service")
+            return
+        }
+
+        val tipPercentage = when (binding.radiogroupService.checkedRadioButtonId) {
+            R.id.radio_amazing -> 20
+            R.id.radio_good -> 18
+            else -> 15
+        }
+
+        val serviceCost = costInput.text.toString().toDouble()
+
+        // Format to 2 decimal places
+        val df = DecimalFormat("#.##")
+
+        // Calculate the tip amount
+        tipAmount = df.format((tipPercentage.toDouble() / 100) * serviceCost).toDouble()
+
+        tipAmountText.text =
+            ((if (roundupToggle.isChecked) round(tipAmount) else tipAmount)).toString()
+        tipAmountText.visibility = View.VISIBLE
+    }
+
+    private fun onRoundSwitchChanged(isChecked: Boolean) {
+        if (tipAmountText.text.toString().isNotEmpty()) {
+            // Get the text from tipAmountText, convert it to a double, round it,
+            // convert it to a String and set the text.
+
+            tipAmountText.text =
+                (if (isChecked) round(
+                    tipAmountText.text.toString().toDouble()
+                ) else tipAmount).toString()
+        }
+    }
+
+    private fun showSnackbar(v: View, msg: String) {
+        Snackbar.make(v, msg, Snackbar.LENGTH_SHORT).show()
+    }
+}
